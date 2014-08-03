ddff299cefcca7c94d797b199fcf2556d7f33606
Joseph Chow <JosephC@linkedin.com>
2014-08-03 11:05:59 -0700

    try

--- history.js
+++ history.js
@@ -5,17 +5,24 @@ function trStr(url, desc, amount) {
   return trStr;
 }
 
-chrome.storage.sync.get(['txHistory'], function(data) {
-  console.log('txHistory data: ', data);
-
-  for (var d in data) {
-    console.log(data[d].host, data[d].message, data[d].amount);
-  }
-});
 
 $(document).ready(function () {
   var str = trStr('url', 'desc', 13);
-  console.log('last tr: ', $('#ht tr:last'))
-  console.log('str: ', str);
-  $('#ht tr:last').after(str);
+//  console.log('last tr: ', $('#ht tr:last'))
+//  console.log('str: ', str);
+
+  chrome.storage.sync.get(['txHistory'], function(data) {
+    console.log('txHistory data: ', data);
+
+    for (var d in data) {
+      var host = data[d].host;
+      var msg = data[d].message;
+      var amount = data[d].amount;
+      
+      console.log(data[d].host, data[d].message, data[d].amount);
+
+      var str = trStr(host, msg, amount);
+      $('#ht tr:last').after(str);
+    }
+  });
 });
