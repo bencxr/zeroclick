e06c779fe01530a460d6d660e7e30935c537f895
Joseph Chow <JosephC@linkedin.com>
2014-08-03 10:59:53 -0700

    try get data

--- history.js
+++ history.js
@@ -5,6 +5,14 @@ function trStr(url, desc, amount) {
   return trStr;
 }
 
+chrome.storage.sync.get(['txHistory'], function(data) {
+  console.log('txHistory data: ', data);
+
+  for (var d in data) {
+    console.log(data[d].host, data[d].message, data[d].amount);
+  }
+});
+
 var str = trStr('url', 'desc', 13);
 console.log('last tr: ', $('#ht tr:last'))
 console.log('str: ', str);
